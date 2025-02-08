3401aa9fde47f966a6dee8671a9402300661e542
Fayline Andrade <93006086+faylineandrade@users.noreply.github.com>
2025-02-07 21:21:54 -0300

    Modifica script principal para processamento de OCR com AWS Textract

--- ocr_lista_escolar/main.py
+++ ocr_lista_escolar/main.py
@@ -1,13 +1,13 @@
 import json
 from pathlib import Path
-
 import boto3
 from botocore.exceptions import ClientError
-from mypy_boto3_textract.type_defs import DetectDocumentTextResponseTypeDef
 
 
 def detect_file_text() -> None:
+
     client = boto3.client("textract")
+    
 
     file_path = str(Path(__file__).parent / "images" / "lista-material-escolar.jpeg")
     with open(file_path, "rb") as f:
@@ -15,21 +15,28 @@ def detect_file_text() -> None:
 
     try:
         response = client.detect_document_text(Document={"Bytes": document_bytes})
+
         with open("response.json", "w") as response_file:
-            response_file.write(json.dumps(response))
+            response_file.write(json.dumps(response, indent=4))
     except ClientError as e:
-        print(f"Erro processando documento: {e}")
+        print(f"Erro ao processar o documento: {e}")
 
 
 def get_lines() -> list[str]:
+   
     try:
         with open("response.json", "r") as f:
-            data: DetectDocumentTextResponseTypeDef = json.loads(f.read())
+            data = json.loads(f.read())
             blocks = data["Blocks"]
-        return [block["Text"] for block in blocks if block["BlockType"] == "LINE"]  # type: ignore
-    except IOError:
+
+        return [block["Text"] for block in blocks if block["BlockType"] == "LINE"]
+    except FileNotFoundError:
+        print("Arquivo response.json não encontrado. Processando a imagem!")
         detect_file_text()
-    return []
+        return get_lines()
+    except Exception as e:
+        print(f"Erro ao ler as linhas: {e}")
+        return []
 
 
 if __name__ == "__main__":
